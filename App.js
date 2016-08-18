799b65fbab3a377861c374cbfa230a3e288e3595
Manuel Carrera <manuel.carrera.91@gmail.com>
2016-08-18 09:09:55 -0600

    setting ui material ui with react js

--- App.js
+++ App.js
@@ -1,8 +1,39 @@
 import React from 'react';
-class App extends React.Component {
+import ReactDOM from 'react-dom';
+import {cyan500} from 'material-ui/styles/colors';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+// This replaces the textColor value on the palette
+// and then update the keys for each component that depends on it.
+// More on Colors: http://www.material-ui.com/#/customization/colors
+const muiTheme = getMuiTheme({
+  palette: {
+    textColor: cyan500,
+  },
+  appBar: {
+    height: 50,
+  },
+});
+
+class Main extends React.Component {
   render() {
-    return <div>Hello world! :)</div>
+    // MuiThemeProvider takes the theme as a property and passed it down the hierarchy
+    // using React's context feature.
+    return (
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <AppBar title="My AppBar" />
+      </MuiThemeProvider>
+    );
   }
 }
 
-export default App
+ReactDOM.render(
+  <Main/>,
+  document.getElementById('app')
+)
+
+export default Main;
